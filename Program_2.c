ded235425d92815963feb5e6cbd76eca0f38886a
Althamash3 <>
2021-05-24 12:25:57 +0000

    Program_2.c

--- Program_2.c
+++ Program_2.c
@@ -10,11 +10,16 @@
 #include<stdio.h>
 int main()
 {
-//Decleade Integer n and write scanf code bellow 
-printf("Enter an Interger Value\n");
+  
+  
+  
+  //Decleade Integer n and write scanf code bellow 
+  int n;
+  printf("Enter an Interger Value\n");
 
-//if you write correcr code your value will print
-printf("%d",n);
-printf("Good Job...");
-return 0;
+  scanf("%d",&n);
+  //if you write correcr code your value will print
+  printf("%d \n",n);
+  printf("Good Job...");
+  return 0;
 }
